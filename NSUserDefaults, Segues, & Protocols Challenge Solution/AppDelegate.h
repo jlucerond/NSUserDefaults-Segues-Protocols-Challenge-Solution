4ad0574fe4d03916ffe2449c4d0ca14af736c292
Joe Lucero <jlucerond@gmail.com>
2015-07-14 14:56:30 -0400

    Initial Commit

---
+++ NSUserDefaults, Segues, & Protocols Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSUserDefaults, Segues, & Protocols Challenge Solution
+//
+//  Created by Joe Lucero on 7/14/15.
+//  Copyright (c) 2015 Joe Lucero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSUserDefaults, Segues, & Protocols Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSUserDefaults, Segues, & Protocols Challenge Solution
+//
+//  Created by Joe Lucero on 7/14/15.
+//  Copyright (c) 2015 Joe Lucero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
